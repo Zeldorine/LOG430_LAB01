14d9497e9d2317d8c67d4af8c083904281a2ce4f
Tony Cazorla <ak98120@ens.etsmtl.ca>
2016-09-15 16:51:21 -0500

    Create model test package

---
+++ src/test/java/model/CardTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import edu.gordon.banking.Card;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Zeldorine
+ */
+public class CardTest {
+
+    public CardTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCreate() {
+        Card card = new Card(1234567889);
+        assertNotNull(card);
+    }
+
+    @Test
+    public void testNumber() {
+        Card card = new Card(1234567889);
+        assertEquals(1234567889, card.getNumber());
+    }
+}
