866f730952ff27d15d1fa9cd136aa4c2d49f6490
Tony Cazorla <ak98120@ens.etsmtl.ca>
2016-09-18 14:16:41 -0500

    Revert "Revert "Add test for the model""
    This reverts commit e173b7739996e02e849e04e6ddad31b14114ebe5.

--- src/test/java/model/MoneyTest.java
+++ src/test/java/model/MoneyTest.java
@@ -65,10 +65,95 @@ public class MoneyTest {
         Money money = new Money(5, 27);
         assertNotNull(money);
         assertEquals("$5.27", money.toString());
+
+        money = new Money(-5, 27);
+        assertNotNull(money);
+        assertEquals("$-4.0-73", money.toString());
+
+        money = new Money(5, -27);
+        assertNotNull(money);
+        assertEquals("$4.73", money.toString());
+
+        money = new Money(-5, -27);
+        assertNotNull(money);
+        assertEquals("$-5.0-27", money.toString());
     }
 
     @Test
     public void testCreateCopy() {
+        Money money = new Money(5, 27);
+        Money copy = new Money(money);
+        assertNotNull(copy);
+        assertEquals("$5.27", copy.toString());
+    }
 
+    @Test
+    public void testAdd() {
+        Money money = new Money(5, 27);
+        Money money1 = new Money(money);
+        money.add(money1);
+        assertEquals("$10.54", money.toString());
+
+        Money money2 = new Money(24, 58);
+        money.add(money2);
+        assertEquals("$35.12", money.toString());
+
+        Money money3 = new Money(24, 58);
+        money3.add(money3);
+        assertEquals("$49.16", money3.toString());
+        
+        Money money4 = new Money(-24, 48);
+        Money money5 = new Money(37, 72);
+        money4.add(money5);
+        assertEquals("$14.20", money4.toString());
+        
+        Money money6 = new Money(-24, -48);
+        Money money7 = new Money(37, -72);
+        money6.add(money7);
+        assertEquals("$11.80", money6.toString());
+    }
+
+    @Test
+    public void testSubstract() {
+        Money money = new Money(5, 27);
+        Money money1 = new Money(money);
+        money.subtract(money1);
+        assertEquals("$0.00", money.toString());
+
+        Money money2 = new Money(24, 48);
+        money.subtract(money2);
+        assertEquals("$-24.0-48", money.toString());
+
+        Money money3 = new Money(24, 48);
+        money3.subtract(money1);
+        assertEquals("$19.21", money3.toString());
+        
+        Money money4 = new Money(24, 48);
+        Money money5 = new Money(37, 72);
+        money4.subtract(money5);
+        assertEquals("$-13.0-24", money4.toString());
+    }
+
+    @Test
+    public void testLessEqual() {
+        Money money = new Money(5, 27);
+        Money copy = new Money(money);
+        assertEquals(true, copy.lessEqual(money));
+        
+        money = new Money(5, 27);
+        Money money1 = new Money(5, 26);
+        assertEquals(true, money1.lessEqual(money));
+        
+        money = new Money(5, 27);
+        money1 = new Money(5, 28);
+        assertEquals(false, money1.lessEqual(money));
+        
+        money = new Money(5, 27);
+        money1 = new Money(-5, 28);
+        assertEquals(true, money1.lessEqual(money));
+                
+        money = new Money(-5, -27);
+        money1 = new Money(-6, 28);
+        assertEquals(true, money1.lessEqual(money));
     }
 }
